731ccf342974a7b5cab4f76ad4fd5afd2b195bdd
dvld <nikoroberts620@gmail.com>
2019-01-07 20:42:15 -0600

    parsing request paths

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 // Primary API file
 
 // dependencies
-const http = require('http');
+var http = require('http');
+var url = require('url');
 
 // server will respond to all requests with a string
-const server = http.createServer(function (req, res) {
+var server = http.createServer(function (req, res) {
+  
+  // get the url and parse it
+  var parsedUrl = url.parse(req.url, true);
+
+  // get the path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // send the response
   res.end('Hello World\n');
+
+    // log the request path
+  console.log('Request received on path ' + trimmedPath);
+
 });
 
 // start server and listen
 server.listen(3000, function () {
   console.log('The server is listening on port 3000');
 });
+
