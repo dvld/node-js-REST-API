519d78861d683b3d2da42803b65aed745409e08c
dvld <nikoroberts620@gmail.com>
2019-01-08 00:12:37 -0600

    setup ping handler route

--- index.js
+++ index.js
@@ -109,10 +109,9 @@ var unifiedServer = function (req, res) {
 // define handlers
 var handlers = {};
 
-// sample handler
-handlers.sample = function (data, callback) {
-  // callback an http status code, and a payload object
-  callback(406, {'name' : 'sample handler'});
+// ping handler
+handlers.ping = function (data, callback) {
+  callback(200);
 };
 
 // not found handler
@@ -122,5 +121,5 @@ handlers.notFound = function (data, callback) {
 
 // define a request router
 var router = {
-  'sample' : handlers.sample
+  'ping' : handlers.ping
 };
